f239ab348f35c3ad08621fddf1894de35dc49395
daopei <252780456@qq.com>
2020-07-07 21:44:43 +0800

    0707

--- week02/work0202/simo.py
+++ week02/work0202/simo.py
@@ -10,7 +10,7 @@ try:
     time.sleep(1)
 
     browser.find_element_by_xpath('//*[@id="root"]/div/div[2]/div/div/div/div[2]/div/div/div[1]/div[1]/div/input').send_keys('13531638637')
-    browser.find_element_by_xpath('/html/body/div[1]/div/div[2]/div/div/div/div[2]/div/div/div[1]/div[2]/div/input').send_keys('norco252780456')
+    browser.find_element_by_xpath('/html/body/div[1]/div/div[2]/div/div/div/div[2]/div/div/div[1]/div[2]/div/input').send_keys('********')
     time.sleep(1)
     browser.find_element_by_xpath('/html/body/div[1]/div/div[2]/div/div/div/div[2]/div/div/div[1]/button').click()
 
